ea9d794af4e77bd12e6eb5c2092fb1640dedc838
d163me <marckelov.danil2014@yandex.ru>
2025-06-14 05:54:41 +0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['hydrich.online']
+    allowedHosts: ['hydrich.online'],
   }
-});
+})
